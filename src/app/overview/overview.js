79a0ffa771d76065b6c621592aec5ac57bbf9ecd
166-mmx <michal@biniek.pl>
2017-01-15 22:30:22 +0100

    [Overview] Added quiz state to block links to quizzes which are not available yet

--- src/app/overview/overview.js
+++ src/app/overview/overview.js
@@ -3,21 +3,46 @@
  */
 class overviewCtrl {
   /**
-   *
    * @param {DataService} DataService common service to fetch data
+   * @param {QuizState}
    * @ngInject
    */
-  constructor(DataService) {
+  constructor(DataService, QuizState) {
     this.DataService = DataService;
+    this.QuizState = QuizState;
   }
 
   $onInit() {
     this.loading = true;
+    this.availableQuizzes = {};
 
     this.DataService.getQuizzes().then(result => {
-      this.quizzes = result.quizzes;
       this.loading = false;
+      this.quizzes = result.quizzes;
+      this._setAvailableQuizzes();
     });
+  }
+
+  _setAvailableQuizzes() {
+    // first quiz always available
+    this.availableQuizzes = {};
+    this.availableQuizzes[this.quizzes[0].id] = true;
+
+    const mostDifficultQuiz = this.QuizState.getMostDifficultResolvedQuiz();
+
+    // assumption that quiz is always available
+    if (mostDifficultQuiz !== 0) {
+      let i = 0;
+      do {
+        i++;
+        this.availableQuizzes[this.quizzes[i].id] = true;
+      } while (this.quizzes[i] !== mostDifficultQuiz);
+
+      if (i < this.quizzes.length - 1) {
+        // add next after saved one
+        this.availableQuizzes[this.quizzes[i + 1].id] = true;
+      }
+    }
   }
 }
 
