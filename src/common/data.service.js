6039f3d38a111553c6c3967dae6d97c671068678
166-mmx <michal@biniek.pl>
2017-01-15 22:12:14 +0100

    [Common] Added comments to data service

--- src/common/data.service.js
+++ src/common/data.service.js
@@ -1,22 +1,36 @@
 /**
+ * Provider of data service
  *
  * @param $resource
  * @param $q
  * @returns {DataService}
- * @constructor
  * @ngInject
  */
 export function DataServiceProvider($resource, $q) {
+
+  /**
+   * Service which is delivering JSON data to application
+   */
   class DataService {
     constructor() {
       this.quizzes = $resource('data/quizzes.json');
       this.questions = $resource('data/questions.json');
     }
 
+    /**
+     * Returns list of available quizzes
+     * @returns {Promise}
+     */
     getQuizzes() {
       return this.quizzes.get().$promise;
     }
 
+    /**
+     * Returns list of questions to requested quiz
+     * @param {number} quizId
+     * @returns {Promise}
+     * @todo add backend validation
+     */
     getQuestions(quizId) {
       //$q.all()
       //return this.questions.get().$promise.then(data => );
